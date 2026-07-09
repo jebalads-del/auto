307611a0c4f748f1f7f923afbe7950da80f6285f
jebalads-del <jebal.ads@gmail.com>
2026-07-09 08:00:59 +0300

    fix: setup next.js

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Auto App",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html>
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
