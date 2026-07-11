33b6550085bd94a003b6349dd59a49a04f85001c
jebalads-del <jebal.ads@gmail.com>
2026-07-11 12:18:53 +0300

    fix: remove i18n config and update setup

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  i18n: {
-    locales: ['ar', 'en'],
-    defaultLocale: 'ar',
-  },
 }
 
 export default nextConfig
