99168d7851403754d3099fd9eefc491c4d5d12d7
jebalads-del <jebal.ads@gmail.com>
2026-07-25 00:39:49 +0300

    حذف import Link غير المستخدم من login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,7 +2,6 @@
 
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
-import Link from 'next/link';
 import Cookies from 'js-cookie';
 
 export default function LoginPage() {
