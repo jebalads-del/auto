3adc0ff15b35218737331547936301231f17c12e
jebalads-del <jebal.ads@gmail.com>
2026-07-31 18:27:37 +0300

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,6 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
-import sql from '../db';
+import sql from '../../../lib/db'; // ✅ المسار الصحيح
 
 export const authOptions = {
   providers: [
