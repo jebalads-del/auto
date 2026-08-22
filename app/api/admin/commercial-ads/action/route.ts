7bd3fd8270e599ba0433261c6e9a239402cd0a04
jebalads-del <jebal.ads@gmail.com>
2026-08-22 08:02:39 +0300

    Update relative backend pathing resolutions to match unified library db location

--- app/api/admin/commercial-ads/action/route.ts
+++ app/api/admin/commercial-ads/action/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
-import sql from '../../../db';
+import sql from '../../../../lib/db';
 
 export async function POST(request: NextRequest) {
   try {
