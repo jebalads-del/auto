a32b101b9d0b2296fd1f7b2a13891db48906b511
jebalads-del <jebal.ads@gmail.com>
2026-08-31 22:32:23 +0300

    fix: remove invalid React import

--- app/car/[id]/page.tsx
+++ app/car/[id]/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState, React } from 'react';
+import { useEffect, useState} from 'react';
 import { useParams, useRouter } from 'next/navigation';
 import Link from 'next/link';
 
